aa0c9ba8daaaefd14a1200ccfffae64fb444bc7d
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-06 15:28:38 +0200

    fase elaborazione dati

--- js/script.js
+++ js/script.js
@@ -11,19 +11,35 @@
 //FASE DI INPUT DATI 
 //utente input km da percorrere
 const km = parseInt(prompt("inserisci kilometri da percorrere"));
-console.log(km, typeof(km));
+console.log("km da percorrere ", km, typeof(km));
 //utente input età
 const eta = parseInt(prompt("inserisci la tua età"));
-console.log(eta, typeof(eta));
+console.log("la tua età ", eta, typeof(eta));
 
 
 
 //FASE ELABORAZIONE DATI
 //init var prezzo base = 0.21*km
+let prz_base = km * 0.21
+console.log("prezzo base è ", prz_base, typeof(prz_base));
 //init var prezzo totale = vuoto
+let prz_tot = 0
+console.log("prezzo totale è ", prz_tot, typeof(prz_tot));
 //if età < 18 then: prz_tot = prz_base * 0.8
-            //else-if età > 65 then: prz_tot = prz_base * 0.6
-                // else: prz_tot = prz_base
+if (eta < 18) {
+    prz_tot = prz_base * 0.8
+    console.log("prezzo totale è ", prz_tot, typeof(prz_tot));
+}
+    //else-if età > 65 then: prz_tot = prz_base * 0.6
+else if (eta > 65) {
+    prz_tot = prz_base * 0.6
+    console.log("prezzo totale è ", prz_tot, typeof(prz_tot));
+}
+        // else: prz_tot = prz_base
+if (18 <= eta <= 65) {
+    prz_tot = prz_base
+    console.log("prezzo totale è ", prz_tot, typeof(prz_tot));
+}
 
 
 //FASE DI OUTPUT
